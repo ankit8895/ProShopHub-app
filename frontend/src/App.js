a8905dfc040658952f5d18319ef9638c628c5169
ankit8895 <125978383+ankit8895@users.noreply.github.com>
2023-06-17 01:58:17 +0530

    Implementing React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomePage />
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/product/:id' element={<ProductPage />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
