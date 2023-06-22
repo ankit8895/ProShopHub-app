b88152574fa6e679c210e180b859879d8c0bfab3
ankit8895 <125978383+ankit8895@users.noreply.github.com>
2023-06-23 00:20:48 +0530

    Bringing redux store into HomePage

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import { productListReducer } from './reducers/productReducers';
-const preloadedState = {
-  productList: productListReducer,
-};
+const preloadedState = {};
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    productList: productListReducer,
+  },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
   devTools: true,
   preloadedState,
