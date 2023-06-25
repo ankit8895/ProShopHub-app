50d465bb7367792c59646072fd32960c47ae1a26
ankit8895 <125978383+ankit8895@users.noreply.github.com>
2023-06-25 23:33:23 +0530

    User authentication endpoint

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,10 +7,6 @@ import {
 } from '../controllers/productController.js';
 
 router.get('/', getProducts);
-
-//@desc Fetch all product
-//@desc GET  /api/product/:id
-//@desc Public
 router.get('/:id', getProductById);
 
 export default router;
