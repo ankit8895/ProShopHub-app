806908f8d8af533edcd966398d0e653f12724f28
ankit8895 <125978383+ankit8895@users.noreply.github.com>
2023-06-25 21:53:10 +0530

    Cart Page

--- frontend/src/redux/reducers/cartReducers.js
+++ frontend/src/redux/reducers/cartReducers.js
@@ -1,20 +1,28 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const addToCart = createAsyncThunk('addToCart', async (productData) => {
-  const { id, qty } = cartData;
-  const response = await axios.get(`/api/products/${id}`);
-  const data = response.data;
+export const addToCart = createAsyncThunk(
+  'addToCart',
+  async (productData, { getState }) => {
+    const { id, qty } = productData;
+    const response = await axios.get(`/api/products/${id}`);
+    const data = response.data;
 
-  return {
-    product: data._id,
-    name: data.name,
-    image: data.image,
-    price: data.price,
-    countInStock: data.countInStock,
-    qty,
-  };
-});
+    localStorage.setItem(
+      'cartItems',
+      JSON.stringify(getState().cart.cartItems)
+    );
+
+    return {
+      product: data._id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      countInStock: data.countInStock,
+      qty,
+    };
+  }
+);
 
 const initialState = {
   loading: false,
@@ -25,34 +33,22 @@ const initialState = {
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
-  reducers: {
-    addCartItem: (state, action) => {
-      const item = action.payload;
-      const existItem = state.cartItems.find((x) => x.product === item.product);
-
-      if (existItem) {
-        return {
-          ...state,
-          cartItems: state.cartItems.map((x) =>
-            x.product === existItem.product ? item : x
-          ),
-        };
-      } else {
-        return {
-          ...state,
-          cartItems: [...state.cartItems, item],
-        };
-      }
-    },
-  },
   extraReducers: (builder) => {
     builder.addCase(addToCart.pending, (state) => {
       state.loading = true;
     });
     builder.addCase(addToCart.fulfilled, (state, action) => {
       state.loading = false;
-      state.cartItems = action.payload;
-      localStorage.setItem('cartItems', JSON.stringify(state.cartItems));
+      const item = action.payload;
+      const existItem = state.cartItems.find((x) => x.product === item.product);
+
+      if (existItem) {
+        state.cartItems = state.cartItems.map((x) =>
+          x.product === existItem.product ? item : x
+        );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
     });
     builder.addCase(addToCart, (state, action) => {
       state.loading = false;
