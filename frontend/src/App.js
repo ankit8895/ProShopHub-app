9715c79de5fe0f18c4bcdb6e398f9aa2a7ef672a
ankit8895 <125978383+ankit8895@users.noreply.github.com>
2023-07-09 01:36:51 +0530

    User edit page and get user details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import PaymentPage from './pages/PaymentPage';
 import PlaceOrderPage from './pages/PlaceOrderPage';
 import OrderPage from './pages/OrderPage';
 import UserListPage from './pages/UserListPage';
+import UserEditPage from './pages/UserEditPage';
 
 const App = () => {
   return (
@@ -32,6 +33,7 @@ const App = () => {
             <Route path='/product/:id' element={<ProductPage />} />
             <Route path='/cart/:id?' element={<CartPage />} />
             <Route path='/admin/userlist' element={<UserListPage />} />
+            <Route path='/admin/user/:id/edit' element={<UserEditPage />} />
             <Route path='/' element={<HomePage />} />
           </Routes>
         </Container>
--- frontend/src/pages/UserListPage.js
+++ frontend/src/pages/UserListPage.js
@@ -74,7 +74,7 @@ const UserListPage = () => {
                   )}
                 </td>
                 <td>
-                  <LinkContainer to={`/user/${user._id}/edit`}>
+                  <LinkContainer to={`/admin/user/${user._id}/edit`}>
                     <Button variant='light' className='btn-sm'>
                       <i className='fa-solid fa-pen-to-square'></i>
                     </Button>
