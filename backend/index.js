944d17299d30ba7f395f33d710a8e7e976a90b4f
ankit8895 <125978383+ankit8895@users.noreply.github.com>
2023-07-11 02:28:31 +0530

    Image Upload Config and Endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import { notFound, errorHandler } from './middleware/errorMiddlleware.js';
@@ -5,6 +6,7 @@ import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 
 dotenv.config();
 
@@ -21,6 +23,7 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/googlepay', (req, res) =>
   res.send({
@@ -28,6 +31,9 @@ app.get('/api/config/googlepay', (req, res) =>
     merchantId: process.env.GOOGLEPAY_MERCHANT_ID,
   })
 );
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(notFound);
 
