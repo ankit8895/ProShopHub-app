95760fd1452637045ef2f933428634d464e67f52
ankit8895 <125978383+ankit8895@users.noreply.github.com>
2023-07-12 22:24:43 +0530

    Morgan and create review endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,8 +2,8 @@ import asyncHandler from 'express-async-handler';
 import Product from '../models/productModel.js';
 
 //@desc Fetch all products
-//@desc GET  /api/products
-//@desc Public
+//@route GET  /api/products
+//@access Public
 
 const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
@@ -11,8 +11,8 @@ const getProducts = asyncHandler(async (req, res) => {
 });
 
 //@desc Fetch all product
-//@desc GET  /api/product/:id
-//@desc Public
+//@route GET  /api/product/:id
+//@access Public
 
 const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
@@ -25,8 +25,8 @@ const getProductById = asyncHandler(async (req, res) => {
 });
 
 //@desc Delete a product
-//@desc DELETE  /api/product/:id
-//@desc Private/Admin
+//@route DELETE  /api/product/:id
+//@access Private/Admin
 
 const deleteProduct = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
@@ -40,8 +40,8 @@ const deleteProduct = asyncHandler(async (req, res) => {
 });
 
 //@desc Create a product
-//@desc POST  /api/products
-//@desc Private/Admin
+//@route POST  /api/products
+//@access Private/Admin
 
 const createProduct = asyncHandler(async (req, res) => {
   const product = new Product({
@@ -61,8 +61,8 @@ const createProduct = asyncHandler(async (req, res) => {
 });
 
 //@desc Update a product
-//@desc PUT  /api/products/:id
-//@desc Private/Admin
+//@route PUT  /api/products/:id
+//@access Private/Admin
 
 const updateProduct = asyncHandler(async (req, res) => {
   const { name, price, description, image, brand, category, countInStock } =
@@ -87,10 +87,54 @@ const updateProduct = asyncHandler(async (req, res) => {
   }
 });
 
+//@desc Create a review
+//@route POST  /api/products/:id/reviews
+//@access Private
+
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (r) => r.user.toString() === req.user._id.toString()
+    );
+
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error('Product already reviewed');
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+
+    product.reviews.push(review);
+
+    product.numReviews = product.reviews.length;
+
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length;
+
+    await product.save();
+
+    res.status(201).json({ message: 'Review added' });
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
 export {
   getProducts,
   getProductById,
   deleteProduct,
   createProduct,
   updateProduct,
+  createProductReview,
 };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,11 +7,13 @@ import {
   deleteProduct,
   createProduct,
   updateProduct,
+  createProductReview,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 router.get('/', getProducts);
 router.post('/', protect, admin, createProduct);
+router.post('/:id/reviews', protect, createProductReview);
 router.get('/:id', getProductById);
 router.delete('/:id', protect, admin, deleteProduct);
 router.put('/:id', protect, admin, updateProduct);
